27727de3c3da80587c0395a6467b9518e6e52c85
Sebastian Lundgren <relinkz@users.noreply.github.com>
2019-04-18 17:40:07 +0200

    Update PixelShader.hlsl

--- MyD3D11Project/PixelShader.hlsl
+++ MyD3D11Project/PixelShader.hlsl
@@ -8,14 +8,14 @@ struct VS_OUTPUT
 float4 main(VS_OUTPUT input) : SV_Target
 {
 	float4 vLightDir = float4(0.0f, 0.0f, -1.0f, 1.0f);
-	float4 vLightColor = float4(0.5f, 0.0f, 0.0f, 1.0f);
+	float4 vLightColor = float4(0.5f, 5.0f, 5.0f, 1.0f);
 
 	//float4 vAmbient = float4();
 
 	//return input.norm;
-	//float4 toReturn = 0.0f;
-	//toReturn +=saturate( dot( (float3)vLightDir, input.norm) * vLightColor);
-
+	float4 toReturn = 0.0f;
+	toReturn +=saturate( dot( (float3)vLightDir, input.norm) * vLightColor);
+	return toReturn + input.color;
 	return float4(abs(input.norm), 0.0f);
 	
 	//return toReturn;
