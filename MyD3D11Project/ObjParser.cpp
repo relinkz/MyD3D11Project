a392afd9acb8a66287b971e2822dc3a3469bbe2d
Sebastian Lundgren <seb.lundgren@gmail.com>
2019-11-23 13:46:59 +0100

    Fix Obj standard
    float3 and float4 should only be needed for the pipeline. If obj file does not specify optional data, these will be set to a default value, as specified in the refacored functions

--- MyD3D11Project/ObjParser.cpp
+++ MyD3D11Project/ObjParser.cpp
@@ -55,73 +55,41 @@ static stringContainer splitStringByCharacter(const string& str, char c)
 	return toReturn;
 }
 
-static vector<array<float, 4>> toFloat4Vector(const stringContainer& geoVertList)
+// non existing optionals are defaulted as defaultValue, Default= 0.0
+static vector<array<float, 4>> toFloat4Vector(const stringContainer& stringList, float defaultValue = 0.0f)
 {
 	vector<array<float, 4>> toReturn;
-	for (const string& row : geoVertList)
+
+	for (const string& row : stringList)
 	{
-		stringContainer split = splitStringByCharacter(row, ' ');
-		if (split.size() != 3 && split.size() != 4)
-		{
-			string err = __func__;
-			err.append(" ObjParser ERROR: geometric vertices not 3 or 4 by row");
-
-			throw(err);
-		}
-
-		array<float, 4> asFloat = { stof(split[0]), stof(split[1]), stof(split[2]), 0.0f };
-		if (split.size() == 4)
-		{
-			asFloat[3] = stof(split[3]);
-		}
-
+		array<float, 4> asFloat = { defaultValue, defaultValue, defaultValue, defaultValue };
 		toReturn.emplace_back(asFloat);
-	}
 
+		stringContainer split = splitStringByCharacter(row, ' ');
+		for (size_t i = 0; i < split.size(); i++)
+		{
+			toReturn.back()[i] = stof(split[i]);
+		}
+	}
 	return toReturn;
 }
 
-static vector<array<float, 3>> toFloat3Vector(const stringContainer& vertexNormalsList)
+// non existing optionals are defaulted as defaultValue, Default= 0.0
+static vector<array<float, 3>> toFloat3Vector(const stringContainer& stringList, float defaultValue = 0.0f)
 {
 	vector<array<float, 3>> toReturn;
 
-	for (const string& row : vertexNormalsList)
+	for (const string& row : stringList)
 	{
-		stringContainer split = splitStringByCharacter(row, ' ');
-		if (split.size() != 3)
-		{
-			string err = __func__;
-			err.append(" ObjParser ERROR: toFloat3Vector");
-
-			throw(err);
-		}
-
-		array<float, 3> asFloat = { stof(split[0]), stof(split[1]), stof(split[2]) };
+		array<float, 3> asFloat = { defaultValue, defaultValue, defaultValue };
 		toReturn.emplace_back(asFloat);
-	}
 
-	return toReturn;
-}
-
-static vector<array<float, 2>> toFloat2Vector(const stringContainer& vertexNormalsList)
-{
-	vector<array<float, 2>> toReturn;
-
-	for (const string& row : vertexNormalsList)
-	{
 		stringContainer split = splitStringByCharacter(row, ' ');
-		if (split.size() != 2)
+		for (size_t i = 0; i < split.size(); i++)
 		{
-			string err = __func__;
-			err.append(" ObjParser ERROR: toFloat2Vector");
-
-			throw(err);
+			toReturn.back()[i] = stof(split[i]);
 		}
-
-		array<float, 2> asFloat = { stof(split[0]), stof(split[1]) };
-		toReturn.emplace_back(asFloat);
 	}
-
 	return toReturn;
 }
 
@@ -131,13 +99,15 @@ ObjParser::ObjParser(const string& src)
 	{
 		const stringContainer rawFileData = storeFileAsString(src);
 
-		const stringContainer verts		= fetchAllStringsOfType("v ", rawFileData);
-		const stringContainer vTexts	= fetchAllStringsOfType("vt ", rawFileData);
-		const stringContainer vNorms	= fetchAllStringsOfType("vn ", rawFileData);
+		const stringContainer v		= fetchAllStringsOfType("v ", rawFileData);
+		const stringContainer vt	= fetchAllStringsOfType("vt ", rawFileData);
+		const stringContainer vn	= fetchAllStringsOfType("vn ", rawFileData);
+		const stringContainer vp	= fetchAllStringsOfType("vp ", rawFileData);
 
-		geometricVertices_				= toFloat4Vector(verts);
-		textureCoordinates_				= toFloat2Vector(vTexts);
-		vertexNormals_					= toFloat3Vector(vNorms);
+		geometricVertices_			= toFloat4Vector(v, 1.0f);
+		textureCoordinates_			= toFloat3Vector(vt);
+		vertexNormals_				= toFloat3Vector(vn);
+		parameterSpaceVertices_		= toFloat3Vector(vp);
 	}
 	catch (const string msg)
 	{
--- MyD3D11Project/ObjParser.h
+++ MyD3D11Project/ObjParser.h
@@ -8,17 +8,21 @@ class ObjParser
 {
 private:
 	std::vector<std::array<float, 4>>	geometricVertices_;
-	std::vector<std::array<float, 3>>	parameterSpaceVertices_;
 	std::vector<std::array<float, 3>>	vertexNormals_;
-	std::vector<std::array<float, 2>>	textureCoordinates_;
+	std::vector<std::array<float, 3>>	textureCoordinates_;
+	std::vector<std::array<float, 3>>	parameterSpaceVertices_;
 public:
 	ObjParser(const std::string& src);
 	ObjParser() = default;
 	~ObjParser() = default;
 
+	//List of geometric vertices, with (x, y, z [,w]) coordinates, w is optional and defaults to 1.0.
 	std::vector<std::array<float, 4>> getGeometricVerticies()		const { return geometricVertices_; }
+	// List of vertex normals in (x,y,z) form; normals might not be unit vectors.
 	std::vector<std::array<float, 3>> getVertexNormals()			const { return vertexNormals_; }
+	// List of texture coordinates, in (u, [,v ,w]) coordinates, these will vary between 0 and 1. v, w are optional and default to 0.
+	std::vector<std::array<float, 3>> getTextureCoordinates()		const { return textureCoordinates_; }
+	// Parameter space vertices in ( u [,v] [,w] ) form; free form geometry statement
 	std::vector<std::array<float, 3>> getParameterSpaceVertices()	const { return parameterSpaceVertices_; }
-	std::vector<std::array<float, 2>> getTextureCoordinates()		const { return textureCoordinates_; }
 };
 
